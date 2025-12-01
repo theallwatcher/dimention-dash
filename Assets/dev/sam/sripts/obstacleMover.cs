c91e726f9f3778982493185d8a4a8a43b64fbcde
theallwatcher <samuelaiden.nap@gmail.com>
2025-12-01 10:19:36 +0100

    bettra fensiens

--- Assets/dev/sam/sripts/obstacleMover.cs
+++ Assets/dev/sam/sripts/obstacleMover.cs
@@ -5,6 +5,8 @@ public class obstacleMover : MonoBehaviour
     //Rigidbody rb;
     //[SerializeField] float speed = -500;
     [SerializeField] Transform endPoint;
+    [SerializeField] GameObject particalPrefab;
+    public bool isCoin = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,6 +30,10 @@ public class obstacleMover : MonoBehaviour
 
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "obstacle") {
             Debug.Log("hit player");
+            if (isCoin == false){
+                GameObject particals = Instantiate(particalPrefab, transform.position, Quaternion.Euler(new Vector3(180, 0, 0)));
+                audioManagerSam.Instance.Play(audioManagerSam.SoundType.Break);
+            }
             Destroy(gameObject);
         }
     }
