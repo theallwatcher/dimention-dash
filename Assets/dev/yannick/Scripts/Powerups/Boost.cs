31515fcc8e67cafd86928fe3daddf4826ae68613
Yannick Garbe <Yannickgarbe02@gmail.com>
2025-12-03 11:12:54 +0100

    eindschermen aangepast

--- Assets/dev/yannick/Scripts/Powerups/Boost.cs
+++ Assets/dev/yannick/Scripts/Powerups/Boost.cs
@@ -5,6 +5,7 @@ public class Boost : BasePowerup
     [SerializeField] float boostAmount;
     protected override void Activate(PlayerMovement player)
     {
+        audioManagerSam.Instance.Play(audioManagerSam.SoundType.Boost);
         player.MovePlayerZ(-boostAmount);
         Destroy(gameObject.transform.parent.gameObject);
     }
